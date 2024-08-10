b38deab13eef90e463067444a48b4e658ec67679
jordanfurr <ejfurr@umich.edu>
2024-08-10 23:44:07 +0200

    photos and home page

---
+++ components/star-header.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <div class="flex flex-row space-between align-center pv3 ph6">
+            <div class="flex flex-row flex-start menu-bar">
+                <p><a href="/" class="menu-item" exact-active-class="active">SHOP</a></p>
+                <p><a href="/" class="menu-item" exact-active-class="active">APPLY</a></p>
+            </div>
+            <div class="text-center">
+                <a href="/"><img class="logo" src="/assets/starlaces.png" alt="star laces llc"></a>
+                
+            </div>
+            <div class="flex flex-end menu-bar">
+                <p><a href="/" class="menu-item" exact-active-class="active">ABOUT</a></p>
+                <!---
+                <p><a href="/cart" class="menu-item" exact-active-class="active">CART</a></p>
+                --->
+            </div>
+        </div>
+    </nav>
+</template>
